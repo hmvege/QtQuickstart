2c15a16e7672300bfd90a17cf57e080bcc3625ef
Mathias <hmvege@ulrik.uio.no>
2018-11-16 20:41:30 +0100

    cleaned up some folders

---
+++ qt_examples/VMC/python_mpi/vmc.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+class VMCSolver:
+    def __init__(self, N_particles):
+        self.N_particles = N_particles
+
+    def run_vmc(self, N_MC):
+        pass
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
